a4ce619af24579fe37ce1fc8b536b910b2b067cc
Jean Mennesson <mennessonj@gmail.com>
2018-11-30 17:53:56 +0100

    Photo Uploader added

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,7 +3,7 @@ import Typed from 'typed.js';
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
     strings: ["Enjoy the best cocktails", "come here"],
-    typeSpeed: 50,
+    typeSpeed: 70,
     loop: true
   });
 }
